186e688370e491bde1778ab38c8467a2be8cf591
James E. Marca <james@activimetrics.com>
2017-11-01 10:14:59 -0700

    test added for post_topic enum type

--- test/post_topic.sql
+++ test/post_topic.sql
@@ -8,5 +8,10 @@ SELECT no_plan();
 
 SELECT pass('Test post_topic!');
 
+SELECT has_type( 'forum_example','post_topic','has post_topic type'  );
+SELECT has_enum( 'forum_example','post_topic','post_topic is an enumerated type'  );
+SELECT enum_has_labels( 'forum_example', 'post_topic', ARRAY[  'discussion', 'inspiration', 'help', 'showcase'],'post_topic has expected labels' );
+
+
 SELECT finish();
 ROLLBACK;
